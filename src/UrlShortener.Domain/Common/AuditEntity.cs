cb007048cc2badcb6747f3778cd779f1fc140d41
Greg DeMaderios <demaderios@gmail.com>
2020-09-03 07:43:08 -0500

    Add AuditEntity model

---
+++ src/UrlShortener.Domain/Common/AuditEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortener.Domain.Common
+{
+    public class AuditEntity
+    {
+        public string sCreatedBy { get; set; }
+        public DateTime dtCreated { get; set; }
+        public string sLastModifiedBy { get; set; }
+        public DateTime? dtLastModified { get; set; }
+    }
+}
