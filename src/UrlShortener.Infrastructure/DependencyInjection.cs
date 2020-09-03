c0a1eb891077225d8431f787888d18ae0835a6f4
Greg DeMaderios <demaderios@gmail.com>
2020-09-02 21:04:14 -0500

    Rename folders, DependencyInjection

---
+++ src/UrlShortener.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Infrastructure.Persistence;
+using UrlShortener.Infrastructure.Identity;
+
+namespace UrlShortener.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
--- src/UrlShortener.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/UrlShortener.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,14 +1,10 @@
-﻿using UrlShortener.UI.Models;
-using IdentityServer4.EntityFramework.Options;
+﻿using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using UrlShortener.UI.Models;
 
-namespace UrlShortener.UI.Data
+namespace UrlShortener.Infrastructure.Persistence
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
